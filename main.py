ed1866e5d77d80e5a9931b3ba5ea5bef8893bede
yotaml <yotaml@jfrog.com>
2021-10-19 10:29:57 +0300

    Added random search for the fit

--- main.py
+++ main.py
@@ -182,13 +182,31 @@ def fit(model: str):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=2)
     clf = None
     if model == 'rf':
-        clf = RandomForestClassifier(criterion='entropy', n_estimators=2000, min_samples_split=5,
-                                     min_samples_leaf=2, max_features='sqrt', bootstrap=True,
-                                     oob_score=True, random_state=2, class_weight='balanced')
+        rf_params = {'n_estimators': [200, 500, 1000],
+                     'max_depth': stats.randint(3, 10),
+                     'max_features': ['auto', 'sqrt'],
+                     'min_samples_split': stats.randint(3, 10),
+                     'min_samples_leaf': stats.randint(1, 5),
+                     'bootstrap': [True, False]}
+        rf = RandomForestClassifier(criterion='entropy', n_estimators=2000, min_samples_split=5,
+                                    min_samples_leaf=2, max_features='sqrt', bootstrap=True,
+                                    oob_score=True, random_state=2, class_weight='balanced')
+        clf = RandomizedSearchCV(estimator=rf, param_distributions=rf_params,
+                                 scoring='average_precision',
+                                 refit=True, random_state=5, cv=4, n_iter=20, verbose=2, n_jobs=-1)
         clf.fit(X_train, y_train)
     elif model == 'etc':
-        clf = ExtraTreesClassifier(n_estimators=2000, min_samples_split=50, min_samples_leaf=20,
+        etc_params = {'n_estimators': [200, 500, 1000],
+                      'max_depth': stats.randint(3, 10),
+                      'max_features': ['auto', 'sqrt'],
+                      'min_samples_split': stats.randint(3, 10),
+                      'min_samples_leaf': stats.randint(1, 5),
+                      'bootstrap': [True, False]}
+        etc = ExtraTreesClassifier(n_estimators=2000, min_samples_split=50, min_samples_leaf=20,
                                    class_weight='balanced', max_features='sqrt', random_state=2)
+        clf = RandomizedSearchCV(estimator=etc, param_distributions=etc_params,
+                                 scoring='average_precision',
+                                 refit=True, random_state=5, cv=4, n_iter=20, verbose=2, n_jobs=-1)
         clf.fit(X_train, y_train)
     elif model == 'cbc':
         catboost_params_grid = {'iterations': [100, 250, 500, 1000],
@@ -198,10 +216,11 @@ def fit(model: str):
                                 'border_count': [5, 10, 20, 50, 100, 200],
                                 'bootstrap_type': ['Bernoulli', 'Bayesian', 'MVS']}
         cbc = CatBoostClassifier(cat_features=get_cat_feature_names(X), auto_class_weights="Balanced", random_state=5,
-                                 bootstrap_type='Bayesian', rsm=0.1, verbose=0)
+                                 bootstrap_type='Bayesian', rsm=0.1, verbose=0, loss_function=FocalLossObjective(),
+                                 eval_metric="Logloss")
         clf = RandomizedSearchCV(estimator=cbc, param_distributions=catboost_params_grid,
-                                    scoring='average_precision',
-                                    refit=True, random_state=42, cv=4, n_iter=20, verbose=2, n_jobs=-1)
+                                 scoring='average_precision',
+                                 refit=True, random_state=5, cv=4, n_iter=20, verbose=2, n_jobs=-1)
         clf.fit(X_train, y_train)
     elif model == 'hist':
         clf = HistGradientBoostingClassifier(categorical_features=get_cat_feature_names(X), verbose=0,
