f0a5865ab65a0c1b688fe5fd93390cc7ef1d9b88
yotaml <yotaml@jfrog.com>
2021-11-01 10:00:42 +0200

    fix

--- main.py
+++ main.py
@@ -375,7 +375,7 @@ def predict():
 
     ### WHAT IF ANALYSIS
     scaler = StandardScaler()
-    bad_accounts = accounts_clean[accounts_clean['rating'] != 'High'].drop('rating', axis=1)
+    bad_accounts = accounts_clean[accounts['rating'] != 'High']
     pred_class_for_train_data = top_model.predict_proba(processed_df_for_fit)[:, 1]
     processed_df_for_fit['class'] = pred_class_for_train_data
     train_data_for_whatif = processed_df_for_fit.loc[processed_df_for_fit['class'] >= high_bar_for_proba, :].drop(
