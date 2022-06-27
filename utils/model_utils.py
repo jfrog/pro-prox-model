635c111d43748268357f2d5226e6d87b422bac97
“seanm92” <iseanmen@gmail.com>
2022-06-27 14:34:29 +0300

    Continue zoom-info migration

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -30,7 +30,7 @@ def cv_evaluation(model, X, y, n_folds=5, n_iter=20, scoring='average_precision'
                            'colsample_bytree': scipy.stats.uniform(loc=0.4, scale=0.6),
                            'reg_alpha': [0, 1e-1, 1, 2, 5, 7, 10, 50, 100],
                            'reg_lambda': [0, 1e-1, 1, 5, 10, 20, 50, 100]}
-        est = LGBMClassifier(class_weight='balanced', random_state=5)
+        est = LGBMClassifier(class_weight='balanced', random_state=5, categorical_feature='auto')
     elif model == 'cbc':
         params = {'iterations': [100, 250, 500, 1000],
                   'learning_rate': stats.uniform(0.01, 0.3),
@@ -56,6 +56,9 @@ def cv_evaluation(model, X, y, n_folds=5, n_iter=20, scoring='average_precision'
         X_selected = X[features_to_keep]
     else:
         X_selected = X.copy()
+
+    if model in ['hist', 'rf']:
+        X_selected = X_selected.select_dtypes(include=np.number)
 
     if model == 'cbc':
         est = CatBoostClassifier(cat_features=get_cat_feature_names(X_selected), auto_class_weights="Balanced", random_state=5,
